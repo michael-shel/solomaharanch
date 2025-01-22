32f912d4cb5ec1aef4f3bf681236630aa833e37b
Mykhailo <michael.shelemba@gmail.com>
2025-01-22 12:37:35 +0000

    main styleguide

---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{html,js,svelte,ts}"],
+  theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    colors: {
+      'dark': '#1f1a15',
+      'white': '#f2ede1',
+      'mint': '#8f9e8d',
+      'brown': '#76563f',
+    },
+    fontFamily: {
+      sans: ['Helvetica', 'sans-serif'],
+    },
+    extend: {
+      spacing: {
+        '128': '32rem',
+        '144': '36rem',
+      },
+      borderRadius: {
+        '4xl': '2rem',
+      }
+    }
+  },
+  plugins: [],
+};
